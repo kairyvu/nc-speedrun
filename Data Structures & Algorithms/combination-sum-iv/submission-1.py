470c8eb4cb5e67eda3cfbe3d3289972c311ffcce
Khoi Vu <kairyvd167@gmail.com>
2026-04-04 22:08:07 -0400

    Add: combination-sum-iv - submission-1

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = defaultdict(int)
+        dp[0] = 1
+        for total in range(1, target + 1):
+            for num in nums:
+                if total - num >= 0:
+                    dp[total] += dp[total - num]
+        return dp[target]
