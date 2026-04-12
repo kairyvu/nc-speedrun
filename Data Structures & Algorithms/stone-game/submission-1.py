d2e8cfa272154a01317b3101a6524e51bf6823a4
Khoi Vu <kairyvd167@gmail.com>
2026-04-12 19:53:08 -0400

    Add: stone-game - submission-1

---
+++ Data Structures & Algorithms/stone-game/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        memo = {}
+        def dfs(i, j):
+            if i > j:
+                return 0
+            if (i, j) in memo:
+                return memo[(i, j)]
+            memo[(i, j)] = max(piles[i] - dfs(i + 1, j), piles[j] - dfs(i, j - 1))
+            return memo[(i, j)]
+        
+        return True if dfs(0, len(piles) - 1) > 0 else False
